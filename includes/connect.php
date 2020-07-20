85d4c8bcd948342b32e6f6c893f6c50cb803dcce
DosuSolomon <53618372+DosuSolomon@users.noreply.github.com>
2020-07-20 01:32:22 +0100

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -1,9 +1,10 @@
+/*mysql://bd5e572a99bd72:d30a54e4@us-cdbr-east-02.cleardb.com/heroku_bbd3b6878b9f671?reconnect=true*/
 <?php
 session_start();
-$servername = "localhost";
-$server_user = "root";
-$server_pass = "";
-$dbname = "food";
+$servername = "us-cdbr-east-02.cleardb.com";
+$server_user = "bd5e572a99bd72";
+$server_pass = "d30a54e4";
+$dbname = "heroku_bbd3b6878b9f671";
 $name = $_SESSION['name'];
 $role = $_SESSION['role'];
 $con = new mysqli($servername, $server_user, $server_pass, $dbname);
